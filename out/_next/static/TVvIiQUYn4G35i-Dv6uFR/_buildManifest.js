80a5e546deb6c3930ccda6fed4662b422dff44eb
arnav-71441 <info@daisforall.com>
2021-08-25 13:50:32 +0530

    Deploy gh-pages

---
+++ out/_next/static/TVvIiQUYn4G35i-Dv6uFR/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/6b9cae372f078ab4a172.css","static/chunks/pages/index-3065565aba85d837e013.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
