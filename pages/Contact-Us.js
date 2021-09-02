a93a7a752ca6b3d3e8b4872ae0bdb707fe3cb483
pranavtalreja <67874105+pranavtalreja@users.noreply.github.com>
2021-09-02 14:48:49 +0530

    done for pages

---
+++ pages/Contact-Us.js
@@ -0,0 +1,14 @@
+export default function ContactUs() {
+     const [login, setLogin] = useState(false);
+     const [register, setRegister] = useState(false);
+     return (
+       <div style={{ width: "100%" }}>
+         <Head></Head>
+         <Navigation></Navigation>
+         <main></main>
+         <Footer></Footer>
+       </div>
+     );
+   }
+   
+   
---
+++ pages/product/[#id].js
@@ -0,0 +1,29 @@
+export default function Product() {
+     const [login, setLogin] = useState(false);
+     const [register, setRegister] = useState(false);
+     return (
+       <div style={{ width: "100%" }}>
+         <Head></Head>
+         <Navigation></Navigation>
+         <main></main>
+         <Footer></Footer>
+       </div>
+     );
+   }
+   
+   export async function getServerSideProps(context) {
+     const client = await clientPromise;
+   
+     // client.db() will be the default database passed in the MONGODB_URI
+     // You can change the database by calling the client.db() function and specifying a database like:
+     // const db = client.db("myDatabase");
+     // Then you can execute queries against your database like so:
+     // db.find({}) or any of the MongoDB Node Driver commands
+   
+     const isConnected = await client.isConnected();
+   
+     return {
+       props: { isConnected },
+     };
+   }
+   
