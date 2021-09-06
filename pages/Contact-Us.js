74aff8632eab5a2d8601888058a8171cb012aa3e
pranavtalreja <67874105+pranavtalreja@users.noreply.github.com>
2021-09-06 18:43:16 +0530

    committed

--- pages/Contact-Us.js
+++ pages/Contact-Us.js
@@ -1,12 +1,15 @@
-import Head from "next/head";
-import { useState } from "react";
-
-export default function ContactUs() {
-  const [login, setLogin] = useState(false);
-  const [register, setRegister] = useState(false);
-  return (
-    <div style={{ width: "100%" }}>
-      <Head></Head>
-    </div>
-  );
+export default function (){
+  return<><div class="blur"></div><div class="content">
+    <h1 class="title">GET IN TOUCH</h1>
+    <div class="contact-info">
+      <ul>
+        <li><div class="circle"><i class="fas fa-home"></i></div> Tokyo, Japan</li>
+        <li><div class="circle"><i class="fas fa-phone"></i></div> +0 000 0000000</li>
+        <li><div class="circle"><i class="fas fa-envelope"></i></div> name@address.com</li>
+        <li><div class="circle"><i class="fas fa-clock"></i></div> Mon - Fri 8:00 AM to 4:00 PM</li>
+      </ul>
+      <div class="contact-section">
+        <input type="text" placeholder="Name"></input>
+      </div>
+    </div></div></>;
 }
